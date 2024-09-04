0b7bee03dd744e23640d80a54ea1fde98e9e29a2
Thilakkumar48 <149754908+Thilakkumar48@users.noreply.github.com>
2024-09-04 09:03:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import json
+from PIL import Image
+
+import numpy as np
+import tensorflow as tf
+import streamlit as st
+
+# Suppress TensorFlow warnings (optional)
+import logging
+tf.get_logger().setLevel(logging.ERROR)
+
+# Define the working directory and model path
+working_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(working_dir, "trained_model", "plant_disease_prediction_model.h5")
+
+# Load the pre-trained model
+model = tf.keras.models.load_model(model_path)
+
+# Load the class names
+class_indices_path = os.path.join(working_dir, "class_indices.json")
+class_indices = json.load(open(class_indices_path))
+
+
+# Function to Load and Preprocess the Image using Pillow
+def load_and_preprocess_image(image_path, target_size=(224, 224)):
+    img = Image.open(image_path)
+    img = img.resize(target_size)
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array.astype('float32') / 255.
+    return img_array
+
+
+# Function to Predict the Class of an Image
+def predict_image_class(model, image_path, class_indices):
+    preprocessed_img = load_and_preprocess_image(image_path)
+    predictions = model.predict(preprocessed_img)
+    predicted_class_index = np.argmax(predictions, axis=1)[0]
+    predicted_class_name = class_indices[str(predicted_class_index)]
+    return predicted_class_name
+
+
+# Streamlit App
+st.title('Plant Disease Classifier')
+
+uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    col1, col2 = st.columns(2)
+
+    with col1:
+        resized_img = image.resize((150, 150))
+        st.image(resized_img)
+
+    with col2:
+        if st.button('Classify'):
+            prediction = predict_image_class(model, uploaded_image, class_indices)
+            st.success(f'Prediction: {str(prediction)}')
+
+##streamlit run c:/Users/DELL/OneDrive/Desktop/Revature/P1/main.py
+
